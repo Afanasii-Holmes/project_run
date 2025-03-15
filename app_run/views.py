efa83c70ba87d5fd2d04f8675c09c3e66122c00f
break-and-enter <clubdedov@gmail.com>
2025-03-15 23:37:06 +0100

    my 5 commit

--- app_run/views.py
+++ app_run/views.py
@@ -1,5 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import viewsets
+from .models import Run
+from .serializers import RunSerializer
 
 
 @api_view(['GET'])
@@ -7,3 +10,7 @@ def company_details(request):
     return Response({'company_name': 'Лососи и барабаны',
                      'slogan':'Табуретки навсегда',
                      'contacts': 'Тел. 222-232-3222'})
+
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
