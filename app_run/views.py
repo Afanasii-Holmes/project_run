644fd92fba1dbf9fa2e5b523a07fc42bcebf605d
break-and-enter <clubdedov@gmail.com>
2025-03-18 19:15:27 +0100

    task 3 solved

--- app_run/views.py
+++ app_run/views.py
@@ -2,7 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import viewsets
 from .models import Run
-from .serializers import RunSerializer
+from .serializers import RunSerializer, UserSerializer
+from django.contrib.auth.models import User
 
 
 @api_view(['GET'])
@@ -14,3 +15,17 @@ def company_details(request):
 class RunViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.all()
     serializer_class = RunSerializer
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.filter(is_superuser=False)
+
+    def get_queryset(self):
+        qs = self.queryset
+        user_type = self.request.query_params.get('type')
+        if user_type and user_type=='coach':
+            qs = qs.filter(is_staff=True)
+        if user_type and user_type=='athlete':
+            qs = qs.filter(is_staff=False)
+        return qs
