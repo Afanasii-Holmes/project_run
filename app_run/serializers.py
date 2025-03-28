e83ad4c8270201f27752c9e214e96c154ff71ef5
break-and-enter <clubdedov@gmail.com>
2025-03-28 18:55:06 +0100

    task 16 - commit 2

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -62,6 +62,7 @@ class ChallengeSerializer(serializers.ModelSerializer):
 
 
 class PositionSerializer(serializers.ModelSerializer):
+    date_time = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%S.%f")
     class Meta:
         model = Position
         fields = '__all__'
