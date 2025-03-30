6b0e4a228cf7eee7be38d001580b0b00f9dd8163
break-and-enter <clubdedov@gmail.com>
2025-03-30 18:37:58 +0200

    task 17 - commit 6 - comment everything

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -57,36 +57,37 @@ class ChallengeSerializer(serializers.ModelSerializer):
 
 
 class PositionSerializer(serializers.ModelSerializer):
-    date_time = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%S.%f")
+    # date_time = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%S.%f")
     class Meta:
         model = Position
-        fields = '__all__'
-
-    def validate(self, data):
-        run = data.get('run')
-        if run.status != 'in_progress':
-            raise serializers.ValidationError('Забег должен быть начат и еще не закончен')
-
-        current_latitude = data.get('latitude')
-        current_longitude = data.get('longitude')
-
-        collectible_items = CollectibleItem.objects.all()
-
-        # for item in collectible_items:
-        #     distance = geodesic((current_latitude,current_longitude), (item.latitude, item.longitude)).meters
-        #     if distance <= 100:
-        #         item.users.add(run.athlete)
-
-        return data
-
-    def validate_latitude(self, value):
-        if value > 90 or value < -90:
-            raise serializers.ValidationError('latitude должен быть в диапазоне от -90.0 до +90.0 градусов')
-        return value
-
-    def validate_longitude(self, value):
-        if value > 180 or value < -180:
-            raise serializers.ValidationError('longitude должен быть в диапазоне от -180.0 до +180.0 градусов')
-        return value
-
-
+        fields = ['id']
+    #     fields = '__all__'
+    #
+    # def validate(self, data):
+    #     run = data.get('run')
+    #     if run.status != 'in_progress':
+    #         raise serializers.ValidationError('Забег должен быть начат и еще не закончен')
+    #
+    #     current_latitude = data.get('latitude')
+    #     current_longitude = data.get('longitude')
+    #
+    #     collectible_items = CollectibleItem.objects.all()
+    #
+    #     for item in collectible_items:
+    #         distance = geodesic((current_latitude,current_longitude), (item.latitude, item.longitude)).meters
+    #         if distance <= 100:
+    #             item.users.add(run.athlete)
+    #
+    #     return data
+    #
+    # def validate_latitude(self, value):
+    #     if value > 90 or value < -90:
+    #         raise serializers.ValidationError('latitude должен быть в диапазоне от -90.0 до +90.0 градусов')
+    #     return value
+    #
+    # def validate_longitude(self, value):
+    #     if value > 180 or value < -180:
+    #         raise serializers.ValidationError('longitude должен быть в диапазоне от -180.0 до +180.0 градусов')
+    #     return value
+    #
+    #
