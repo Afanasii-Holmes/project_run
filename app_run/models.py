1cf4ff0103e1995aedfdcf785d4d034d16dcac06
break-and-enter <clubdedov@gmail.com>
2025-06-07 16:50:28 +0200

    change from auto_now to auto_now_add in created_at field in model Run

--- app_run/models.py
+++ app_run/models.py
@@ -10,7 +10,7 @@ STATUS_CHOICES = [
 
 
 class Run(models.Model):
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     comment = models.TextField()
     athlete = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=30, choices=STATUS_CHOICES, default='init')
